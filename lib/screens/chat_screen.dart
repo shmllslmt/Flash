e56f7a48e910e9b3a147cd47f87f373d942c9423
shmll <shmllslmt@gmail.com>
2023-04-18 14:40:21 +0800

    Chat StreamBuilder done

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flash/constants.dart';
+
+User? loggedInUser;
 
 class ChatScreen extends StatefulWidget {
   static String id = 'chat_screen';
@@ -13,12 +16,32 @@ class _ChatScreenState extends State<ChatScreen> {
   //Create another private FirebaseAuth instance
   final _auth = FirebaseAuth.instance;
 
-  //TODO: Step 29 - Create a Firestore instance
-  //TODO: Step 34 - Create a TextEditingController
+  //Create a Firestore instance
+  final _firestore = FirebaseFirestore.instance;
 
-  //TODO: Step 24 - Create a getCurrentUser() method that will check whether a user is signed in
+  String? messageText;
 
-  //TODO: Step 25 - Override the initState() method to call the getCurrentUser()
+  //Create a TextEditingController
+  final messageController = TextEditingController();
+
+  //Create a getCurrentUser() method that will check whether a user is signed in
+  void getCurrentUser() {
+    final user = _auth.currentUser;
+
+    if(user != null) {
+      loggedInUser = user;
+
+      print(loggedInUser!.email);
+    }
+  }
+
+  //Override the initState() method to call the getCurrentUser()
+  @override
+  void initState() {
+    super.initState();
+
+    getCurrentUser();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,10 +68,39 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            //TODO: Step 30 - Create a StreamBuilder<QuerySnapshot<Map<String, dynamic>>> widget
-            //TODO: Step 31 - For the stream property, _firestore.collection('messages').orderBy('timestamp').snapshots()
-            //TODO: Step 32 - If snapshot has data, create a list of Text widgets - the Text widgets will contain the messages, then return a ListView with the list of Text widgets as children
-            //TODO: Step 33 - If snapshot has no data, return a CircularProgressIndicator
+            //Create a StreamBuilder<QuerySnapshot<Map<String, dynamic>>> widget
+            StreamBuilder(
+              //For the stream property, _firestore.collection('messages').orderBy('timestamp').snapshots()
+              stream: _firestore.collection('messages').orderBy('timestamp').snapshots(),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if(!snapshot.hasData) {
+                  //If snapshot has no data, return a CircularProgressIndicator
+                  return Container(
+                    child: CircularProgressIndicator(
+                      color: Colors.lightBlueAccent,
+                    ),
+                  );
+                }
+                  else {
+                  //If snapshot has data, create a list of Text widgets - the Text widgets will contain the messages, then return a ListView with the list of Text widgets as children
+                  var messages = snapshot.data.docs;
+                  List<Text> messageWidgets = [];
+
+                  for(var message in messages) {
+                    final messageText = message.data()['text'];
+                    final messageSender = message.data()['sender'];
+
+                    final messageWidget = Text('$messageText from $messageSender', style: TextStyle(color: Colors.white),);
+                    messageWidgets.add(messageWidget);
+                  }
+                  return Expanded(
+                    child: ListView(
+                      children: messageWidgets,
+                    ),
+                  );
+                }
+              },
+            ),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -56,14 +108,26 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: [
                   Expanded(
                     child: TextField(
-                      onChanged: (value) {},
+                      controller: messageController,
+                      style: TextStyle(color: Colors.white),
+                      onChanged: (value) {
+                        messageText = value;
+                      },
                       decoration: kMessageTextFieldDecoration,
                     ),
                   ),
                   TextButton(
-                    //TODO: Step 29 - Add data (messageText, sender, timestamp) to the collection
-                    //TODO: Step 35 - Clear the TextEditingController
-                    onPressed: () {},
+                    onPressed: () {
+                      //Clear the TextEditingController
+                      messageController.clear();
+
+                      //Add data (messageText, sender, timestamp) to the collection
+                      _firestore.collection('messages').add({
+                        'sender': loggedInUser!.email,
+                        'text': messageText,
+                        'timestamp': FieldValue.serverTimestamp(),
+                      });
+                    },
                     child: Text(
                       'Send',
                       style: kSendButtonTextStyle,
